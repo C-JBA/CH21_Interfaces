605e46bb852c4ecfc5e6ec3adbcb38fdc098f938
CJBA <barajasavilacristobaljair@gmail.com>
2023-04-27 10:21:36 -0600

    Se importan clases al archivo de test

--- testFiguras.java
+++ testFiguras.java
@@ -1,5 +1,4 @@
-import org.generation.figuras.Triangulo;
-import org.generation.printer.*;
+import org.generation.figuras.*;
 import org.generation.printer.*;
 import org.generation.util.*;
 
@@ -7,11 +6,14 @@ public class testFiguras {
 	public static void main(String[] args) {
 		Triangulo t1 = new  Triangulo("Triángulo t1", 20.0, 10.0, 15.0);
 		Triangulo t2 = new  Triangulo("Bermudas", 87.0, 121.0, 101.0);
+		
+		Cuadrado c1 = new Cuadrado("Cuadrado",4);
 		// TODO: Aquí se deben instanciar las otras figuras geométricas
 		
 		// TODO: Aquí se debe probar cada figura con el método imprimirCalculo
 		Printer.imprimirCalculo(t1);
 		Printer.imprimirCalculo(t2);
+		Printer.imprimirCalculo(c1);
 	}//main
 
 	
